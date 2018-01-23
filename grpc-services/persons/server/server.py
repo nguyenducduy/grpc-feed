25c5977591afec61f3a035075da0ae6019ef775a
Nguyen Duy <duy@olli-ai.com>
2018-01-23 17:13:17 +0700

    no message

--- grpc-services/persons/server/server.py
+++ grpc-services/persons/server/server.py
@@ -4,12 +4,23 @@ import grpc
 import persons_pb2_grpc as persons_service
 import persons_pb2 as persons_messages
 from simplemysql import SimpleMysql
-from stream_framework.feeds.cassandra import CassandraFeed
+from stream_framework.feeds.redis import RedisFeed
 from stream_framework.feed_managers.base import Manager
+from stream_framework.activity import Activity
+from stream_framework.verbs import register
+from stream_framework.verbs.base import Verb
+from datetime import datetime
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-class PostFeed(CassandraFeed):
+class Pin(Verb):
+    id = 5
+    infinitive = 'pin'
+    past_tense = 'pinned'
+
+register(Pin)
+
+class PostFeed(RedisFeed):
     key_format = 'feed:normal:%(user_id)s'
 
 class UserPostFeed(PostFeed):
@@ -50,15 +61,16 @@ class PersonsService(persons_service.PersonsServicer):
     def GetPersons(self, request, context):
         print('get persons call')
         feed = UserPostFeed(13)
-        print(feed)
-        # activity = Activity(
-        #     actor=13,
-        #     verb=1,
-        #     object=1, # The id of the newly created Pin object
-        #     target=1,
-        #     time=datetime.utcnow()
-        # )
-        # feed.add(activity)
+
+        activity = Activity(
+            actor=13,
+            verb=Pin,
+            object=1, # The id of the newly created Pin object
+            target=1,
+            time=datetime.utcnow()
+        )
+
+        feed.add(activity)
 
         # persons = self.db.getAll('pr_person', ['pe_id', 'pe_full_name'])
         # # print(persons)
